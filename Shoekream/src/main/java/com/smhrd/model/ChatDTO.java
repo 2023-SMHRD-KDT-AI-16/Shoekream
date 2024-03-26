c3582f8f4375cfd9b0990f0e31b2129d71e2d3ab
SaveJobs <jijaeng@gmail.com>
2024-03-26 09:18:19 +0900

    chatDTO와 ChatMapper 수정

--- Shoekream/src/main/java/com/smhrd/model/ChatDTO.java
+++ Shoekream/src/main/java/com/smhrd/model/ChatDTO.java
@@ -1,43 +1,61 @@
 package com.smhrd.model;
 
-
+import java.sql.Timestamp; // Timestamp 타입을 사용하기 위한 import
 
 public class ChatDTO {
-	
-	int chatID;
-	String fromID;
-	String toID;
-	String chatContent;
-	String chatTime;
-	
-	public int getChatID() {
-		return chatID;
-	}
-	public void setChatID(int chatID) {
-		this.chatID = chatID;
-	}
-	public String getFromID() {
-		return fromID;
-	}
-	public void setFromID(String fromID) {
-		this.fromID = fromID;
-	}
-	public String getToID() {
-		return toID;
-	}
-	public void setToID(String toID) {
-		this.toID = toID;
-	}
-	public String getChatContent() {
-		return chatContent;
-	}
-	public void setChatContent(String chatContent) {
-		this.chatContent = chatContent;
-	}
-	public String getChatTime() {
-		return chatTime;
-	}
-	public void setChatTime(String chatTime) {
-		this.chatTime = chatTime;
-	}
+    
+    private int chatID; // chatID는 숫자 타입으로 변경
+    private int roomIdx; // 채팅방 인덱스 추가
+    private String fromID;
+    private String toID;
+    private String chatContent;
+    private Timestamp chatTime; // java.sql.Timestamp 타입으로 변경
+
+    // Getters and Setters
+    public int getChatID() {
+        return chatID;
+    }
+
+    public void setChatID(int chatID) {
+        this.chatID = chatID;
+    }
+    public int getRoomIdx() { // roomIdx getter
+        return roomIdx;
+    }
+
+    public void setRoomIdx(int roomIdx) { // roomIdx setter
+        this.roomIdx = roomIdx;
+    }
+
+    public String getFromID() {
+        return fromID;
+    }
+
+    public void setFromID(String fromID) {
+        this.fromID = fromID;
+    }
+
+    public String getToID() {
+        return toID;
+    }
+
+    public void setToID(String toID) {
+        this.toID = toID;
+    }
+
+    public String getChatContent() {
+        return chatContent;
+    }
+
+    public void setChatContent(String chatContent) {
+        this.chatContent = chatContent;
+    }
+
+    public Timestamp getChatTime() {
+        return chatTime;
+    }
+
+    public void setChatTime(Timestamp chatTime) {
+        this.chatTime = chatTime;
+    }
 }
