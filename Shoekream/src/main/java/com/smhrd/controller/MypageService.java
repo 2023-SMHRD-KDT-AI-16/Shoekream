d5b38189b31b6e7f32cca25490e4465284b4d108
User <User@192.168.219.158>
2024-03-29 00:36:31 +0900

    MypageService 생성

---
+++ Shoekream/src/main/java/com/smhrd/controller/MypageService.java
@@ -0,0 +1,21 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MypageService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("MypageService 도착");
+		//System.out.println(postuser_id);
+		
+		
+	}
+
+}
