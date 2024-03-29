e48f43ce583e3fd729081069f7c77fdfeb058445
User <User@192.168.219.158>
2024-03-29 16:40:16 +0900

    게시글 작성 서비스 수정

--- Shoekream/src/main/java/com/smhrd/controller/WriterService.java
+++ Shoekream/src/main/java/com/smhrd/controller/WriterService.java
@@ -27,35 +27,38 @@ public class WriterService extends HttpServlet {
 		//게시글 작성 속성 가져오기 
 		request.setCharacterEncoding("UTF-8");
 		
-		//파일 업로드를 위한 multi 불러오기 
-		String path ="C:\\Users\\User\\git\\Shoekream\\Shoekream\\src\\main\\webapp\\post_img";
-		int maxSize = 1024*1024*7;
-		String encoding = "UTF-8";
-		DefaultFileRenamePolicy rename= new DefaultFileRenamePolicy();
+		//신발정보 불러오기
+		String[] selectedShoes = request.getParameterValues("selectedShoes");
 		
-		MultipartRequest multi = new MultipartRequest(request,path,maxSize,encoding,rename);
-		String filename= URLEncoder.encode(multi.getFilesystemName("filename"),"UTF-8");
-		String content = multi.getParameter("content");
-		System.out.println(content+":"+filename);
-		
-		//-----------------------------------------------------------
-		HttpSession session = request.getSession();
-		UserDTO user_info = (UserDTO) session.getAttribute("user_info");
-		BoardDAO dao = new BoardDAO();
-		BoardDTO dto = new BoardDTO();
-		dto.setPostContent(content);
-		dto.setPostImg(filename);
-		dto.setUserId(user_info.getUserId());
-		int result = dao.upload(dto);
-		
-		if(result>0) {
-			System.out.println("게시글 등록 성공!");
-		}else {
-			System.out.println("등록 실패");
-		}
-		
-		response.sendRedirect("Main.jsp");
-	
+//		//파일 업로드를 위한 multi 불러오기 
+//		String path ="C:\\Users\\User\\git\\Shoekream\\Shoekream\\src\\main\\webapp\\post_img";
+//		int maxSize = 1024*1024*7;
+//		String encoding = "UTF-8";
+//		DefaultFileRenamePolicy rename= new DefaultFileRenamePolicy();
+//		
+//		MultipartRequest multi = new MultipartRequest(request,path,maxSize,encoding,rename);
+//		String filename= URLEncoder.encode(multi.getFilesystemName("filename"),"UTF-8");
+//		String content = multi.getParameter("content");
+//		System.out.println(content+":"+filename);
+//		
+//		//-----------------------------------------------------------
+//		HttpSession session = request.getSession();
+//		UserDTO user_info = (UserDTO) session.getAttribute("user_info");
+//		BoardDAO dao = new BoardDAO();
+//		BoardDTO dto = new BoardDTO();
+//		dto.setPostContent(content);
+//		dto.setPostImg(filename);
+//		dto.setUserId(user_info.getUserId());
+//		int result = dao.upload(dto);
+//		
+//		if(result>0) {
+//			System.out.println("게시글 등록 성공!");
+//		}else {
+//			System.out.println("등록 실패");
+//		}
+//		
+//		response.sendRedirect("Main.jsp");
+//	
 	}
 
 }
